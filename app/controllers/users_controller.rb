04f39ac5719d7af61be8a4af1efa328b39f35aa3
Sean Padden <you@example.com>
2019-09-19 14:11:19 -0400

    First commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  # skip_before_action :authorized?, only: [:create, :index]
+
+  def index 
+    users = User.all
+    render json: users
+  end
+
+  def profile
+    render json: current_user
+  end
+    
+  def create
+    user = User.create(user_params)
+    if user.valid?
+      render json: {token: create_token(user.id)}
+    else
+      render json: {errors: user.errors.full_messages}, status: 422
+    end
+  end
+
+  private 
+
+  def user_params
+    params.permit(:username, :password)
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/users', to: 'users#index'
+
+  post '/login', to: 'auth#login'
+  post '/signup', to: 'users#create'
+  get '/profile', to: 'users#profile'
+
+  
+end  
