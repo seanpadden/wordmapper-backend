b3a67cad4389b5d22040d4449aa76cec096f5cde
Sean Padden <you@example.com>
2019-10-01 13:00:58 -0400

    usermap functionality working

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,2 +1,23 @@
 class MapsController < ApplicationController
+
+  def index
+    maps = Map.all
+    render json: maps
+  end 
+
+  def create 
+    map = Map.create(map_params)
+    if map.valid?
+      render json: map
+    else 
+      render json message: 'failed to create map! are you logged in?'
+    end 
+  end 
+
+  private 
+
+  def map_params
+    params.permit(:word_name, :etymology, :lat, :lng, :user_id)
+  end
+
 end
--- app/models/map.rb
+++ app/models/map.rb
@@ -1,3 +1,4 @@
 class Map < ApplicationRecord
   belongs_to :user
+  validates :word_name, :etymology, :lat, :lng, :user_id, presence: true
 end
