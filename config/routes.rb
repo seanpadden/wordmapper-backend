79f560b9510df58dc5f5c3c3f264f5ab2d428d8f
Sean Padden <you@example.com>
2019-10-09 13:03:04 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :maps
   resources :words
   resources :coordinates
+  resources :users 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/users', to: 'users#index'
 
