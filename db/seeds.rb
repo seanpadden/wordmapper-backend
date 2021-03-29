bc70c658ac33691716198bbe3e08ceb591e6356c
Sean Padden <sean.padden7@gmail.com>
2021-03-29 17:32:21 -0400

    clean up seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,86 @@
 # end 
 
 Language.destroy_all 
-Location.destroy_all
+ActiveRecord::Base.connection.reset_pk_sequence!('languages')
 
-en = Language.create(name: "English", country: "England")
-Location.create(lat: 54.0000, lng: -2.0000, language: en)
+Location.destroy_all
+ActiveRecord::Base.connection.reset_pk_sequence!('locations')
+
+# en = Language.create(name: "English", country: "England")
+# Location.create(lat: 54.0000, lng: -2.0000, language: en)
+
+obj = {
+  English: {lat: 54.0000, lng: -2.0000},
+  "English,": {lat: 54.0000, lng: -2.0000},
+  "Anglo-French": {lat: 46.0000, lng: 2.0000},
+  "Anglo-French,": {lat: 46.0000, lng: 2.0000},
+  French: {lat: 46.0000, lng: 2.0000},
+  "French,": {lat: 46.0000, lng: 2.0000},
+  German: {lat: 51.1657, lng: 10.4515},
+  "German,": {lat: 51.1657, lng: 10.4515},
+  Germanic: {lat: 51.1657, lng: 10.4515},
+  "Germanic,": {lat: 51.1657, lng: 10.4515},
+  Goth: {lat: 51.1657, lng: 10.4515},
+  "Goth,": {lat: 51.1657, lng: 10.4515},
+  Gothic: {lat: 51.1657, lng: 10.4515},
+  "Gothic,": {lat: 51.1657, lng: 10.4515},
+  Dutch: {lat: 52.1326, lng: 5.2913},
+  "Dutch,": {lat: 52.1326, lng: 5.2913},
+  Greek: {lat: 37.9838, lng: 23.7275},
+  "Greek,": {lat: 37.9838, lng: 23.7275},
+  Latin: {lat: 41.8719, lng: 12.5674},
+  "Latin,": {lat: 41.8719, lng: 12.5674},
+  Italian: {lat: 41.8719, lng: 12.5674},
+  "Italian,": {lat: 41.8719, lng: 12.5674},
+  Spanish: {lat: 40.4637, lng: 3.7492},
+  "Spanish,": {lat: 40.4637, lng: 3.7492},
+  Castilian: {lat: 40.4637, lng: 3.7492},
+  "Castilian,": {lat: 40.4637, lng: 3.7492},
+  Egyptian: {lat: 26.8206, lng: 30.8025},
+  "Egyptian,": {lat: 26.8206, lng: 30.8025},
+  Nordic: {lat: 60.4720, lng: 8.4689},
+  "Nordic,": {lat: 60.4720, lng: 8.4689},
+  Norwegian: {lat: 60.4720, lng: 8.4689},
+  "Norwegian,": {lat: 60.4720, lng: 8.4689},
+  Norse: {lat: 60.4720, lng: 8.4689},
+  "Norse,": {lat: 60.4720, lng: 8.4689},
+  Arabic: {lat: 23.8859, lng: 45.0792},
+  "Arabic,": {lat: 23.8859, lng: 45.0792},
+  Gaelic: {lat: 53.4129, lng: 8.2439},
+  'Gaelic,': {lat: 53.4129, lng: 8.2439},
+  Russian: {lat: 61.5240, lng: 105.3188},
+  "Russian,": {lat: 61.5240, lng: 105.3188},
+  Chinese: {lat: 35.8617, lng: 104.1954},
+  "Chinese,": {lat: 35.8617, lng: 104.1954},
+  Sanskrit: {lat: 35.8617, lng: 104.1954}, 
+  "Sanskrit,": {lat: 35.8617, lng: 104.1954}, 
+  Hindi: {lat: 20.5937, lng: 78.9629},
+  "Hindi,": {lat: 20.5937, lng: 78.9629},
+  "Old Church Slavonic": {lat: 48.6690, lng: 19.6990},
+  "Old Church Slavonic,": {lat: 48.6690, lng: 19.6990},
+  Slavonic: {lat: 48.6690, lng: 19.6990},
+  Hebrew: {lat: 31.7683, lng: 35.2137},
+  "Hebrew,": {lat: 31.7683, lng: 35.2137},
+  Yiddish: {lat: 51.1657, lng: 10.4515},
+  "Yiddish,": {lat: 51.1657, lng: 10.4515},
+  Lithuanian: {lat: 55.1694, lng: 23.8813},
+  "Lithuanian,": {lat: 55.1694, lng: 23.8813},
+  Turkish: {lat: 38.9637, lng: 35.2433},
+  "Turkish,": {lat: 38.9637, lng: 35.2433},
+  Algonquian: {lat: 45.5017, lng: -73.5673},
+  'Algonquian,': {lat: 45.5017, lng: -73.5673},
+  Nahuatl: {lat: 20.683056, lng: -88.568611},
+  "Nahuatl,": {lat: 20.683056, lng: -88.568611},
+  Louisiana: {lat: 29.9511, lng: -90.0715},
+  Haitian: {lat: 18.9712, lng: -72.2852},
+  Bantu: {lat: 7.3775, lng: 3.9470},
+  Kimbundu: {lat: -11.2027, lng: 17.8739}
+}
+
+keys = obj.keys
+keys.map do |el| 
+  newLang = Language.create(name: el.to_s, country: "")
+  Location.create(lat: obj[el][:lat], lng: obj[el][:lng], language: newLang)  
+end 
 
 puts "get seeded"
